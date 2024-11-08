9ddca0bd17b1528e1dea8add90aa3e8838c30cd0
Moonbjin <qudwls5778@naver.com>
2024-11-09 02:17:28 +0900

    fix: Resolve conflicts between timer and stopwatch functions

--- lib/components/pomodoro_timer.dart
+++ lib/components/pomodoro_timer.dart
@@ -4,10 +4,10 @@ import 'package:percent_indicator/percent_indicator.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '../constants/icons.dart';
 import '../services/timer_service.dart';
+import '../services/stopwatch_service.dart';
 
 class PomodoroTimer extends StatefulWidget {
   final int duration;
-
   const PomodoroTimer({Key? key, required this.duration}) : super(key: key);
 
   @override
@@ -16,20 +16,26 @@ class PomodoroTimer extends StatefulWidget {
 
 class _PomodoroTimerState extends State<PomodoroTimer> {
   late TimerService _timerService;
+  late StopwatchService _stopwatchService;
   late PageController _pageController;
   int _currentPageIndex = 0; // 현재 페이지 인덱스
 
   @override
   void initState() {
     super.initState();
-    _timerService = TimerService(duration: widget.duration, mode: TimerMode.countdown)
+    _timerService = TimerService(duration: widget.duration)
       ..onTick = () {
-        setState(() {}); // 타이머 또는 스탑워치 업데이트 시 UI 갱신
+        setState(() {});
       }
       ..onComplete = () {
         setState(() {
           _timerService.isRunning = false;
         });
+      };
+
+    _stopwatchService = StopwatchService()
+      ..onTick = () {
+        setState(() {});
       };
 
     _pageController = PageController(initialPage: 0);
@@ -38,6 +44,7 @@ class _PomodoroTimerState extends State<PomodoroTimer> {
   @override
   void dispose() {
     _timerService.dispose();
+    _stopwatchService.dispose();
     _pageController.dispose();
     super.dispose();
   }
@@ -46,18 +53,20 @@ class _PomodoroTimerState extends State<PomodoroTimer> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const SizedBox(height: 10),
         const SizedBox(height: 40),
-        // 페이지 인디케이터 추가
         _buildPageIndicator(),
-        Expanded(
+        Container(
+          height: 500,
           child: PageView(
             controller: _pageController,
             onPageChanged: (index) {
               setState(() {
                 _currentPageIndex = index;
-                _timerService.mode = index == 0 ? TimerMode.countdown : TimerMode.stopwatch;
-                _timerService.reset();
+                if (index == 0) {
+                  _timerService.reset();
+                } else {
+                  _stopwatchService.reset();
+                }
               });
             },
             children: [
@@ -66,12 +75,13 @@ class _PomodoroTimerState extends State<PomodoroTimer> {
             ],
           ),
         ),
+        const SizedBox(height: 10),
         buildSelectBook(context),
-        const SizedBox(height: 19),
+        const SizedBox(height: 20),
       ],
     );
   }
-  // 페이지 인디케이터 위젯
+
   Widget _buildPageIndicator() {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
@@ -89,15 +99,15 @@ class _PomodoroTimerState extends State<PomodoroTimer> {
       }),
     );
   }
+
   Widget _buildPomodoroPage() {
     final double screenWidth = MediaQuery.of(context).size.width;
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Align(
           alignment: Alignment.topLeft,
           child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05, vertical: 10),
+            padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.1, vertical: 1),
             child: Text(
               '뽀모도로',
               style: TextStyle(
@@ -109,124 +119,129 @@ class _PomodoroTimerState extends State<PomodoroTimer> {
             ),
           ),
         ),
-        const SizedBox(height: 40),
-        CircularPercentIndicator(
-          radius: 114.5,
-          lineWidth: 20.0,
-          percent: _timerService.progress,
-          center: Text(
-            _timerService.formatTime(),
-            style: const TextStyle(fontFamily: "SUITE", fontSize: 44, fontWeight: FontWeight.w800),
-          ),
-          progressColor: AppColors.pointColor,
-          backgroundColor: Colors.grey[300]!,
-          circularStrokeCap: CircularStrokeCap.round,
-          reverse: true,
-        ),
         const SizedBox(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              children: [
-                IconButton(
-                  icon: SvgPicture.asset(AppIcons.restartIcon),
-                  iconSize: 30,
-                  onPressed: () {
-                    setState(() {
-                      _timerService.reset();
-                    });
-                  },
+        Expanded(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularPercentIndicator(
+                radius: 114.5,
+                lineWidth: 20.0,
+                percent: _timerService.progress,
+                center: Text(
+                  _timerService.formatTime(),
+                  style: const TextStyle(fontFamily: "SUITE", fontSize: 44, fontWeight: FontWeight.w800),
                 ),
-                const Text(
-                  "다시 시작",
-                  style: TextStyle(fontFamily: "SUITE", fontSize: 12, color: Colors.grey, fontWeight: FontWeight.w600),
-                ),
-              ],
-            ),
-            const SizedBox(width: 20),
-            Column(
-              children: [
-                IconButton(
-                  icon: Icon(_timerService.isRunning ? Icons.pause : Icons.play_arrow),
-                  iconSize: 30,
-                  onPressed: () {
-                    setState(() {
-                      if (_timerService.isRunning) {
-                        _timerService.stop();
-                      } else {
-                        _timerService.start();
-                      }
-                    });
-                  },
-                ),
-                Text(
-                  _timerService.isRunning ? "정지" : "시작",
-                  style: const TextStyle(fontFamily: "SUITE", fontSize: 12, color: Colors.grey, fontWeight: FontWeight.w600),
-                ),
-              ],
-            ),
-          ],
+                progressColor: AppColors.pointColor,
+                backgroundColor: Colors.grey[300]!,
+                circularStrokeCap: CircularStrokeCap.round,
+                reverse: true,
+              ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Column(
+                    children: [
+                      IconButton(
+                        icon: SvgPicture.asset(AppIcons.restartIcon),
+                        iconSize: 30,
+                        onPressed: () {
+                          setState(() {
+                            _timerService.reset();
+                          });
+                        },
+                      ),
+                      const Text(
+                        "다시 시작",
+                        style: TextStyle(fontFamily: "SUITE", fontSize: 12, color: Colors.grey, fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(width: 40),
+                  Column(
+                    children: [
+                      IconButton(
+                        icon: SvgPicture.asset(_timerService.isRunning ? AppIcons.pauseIcon : AppIcons.startIcon),
+                        iconSize: 30,
+                        onPressed: () {
+                          setState(() {
+                            if (_timerService.isRunning) {
+                              _timerService.stop();
+                            } else {
+                              _timerService.start();
+                            }
+                          });
+                        },
+                      ),
+                      Text(
+                        _timerService.isRunning ? "정지" : "시작",
+                        style: const TextStyle(fontFamily: "SUITE", fontSize: 12, color: Colors.grey, fontWeight: FontWeight.w600),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ],
     );
   }
+
   Widget _buildStopwatchPage() {
+    final double screenWidth = MediaQuery.of(context).size.width;
     return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        Align(
+          alignment: Alignment.topLeft,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.1, vertical: 1),
+            child: Text(
+              '스탑워치',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 24,
+                fontWeight: FontWeight.w800,
+                fontFamily: 'SUITE',
+              ),
+            ),
+          ),
+        ),
+        const SizedBox(height: 20),
         Text(
-          _timerService.formatTime(),
+          _stopwatchService.formatTime(),
           style: const TextStyle(fontFamily: "SUITE", fontSize: 44, fontWeight: FontWeight.w800),
         ),
         const SizedBox(height: 20),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Column(
-              children: [
-                IconButton(
-                  icon: Icon(Icons.refresh),
-                  iconSize: 30,
-                  onPressed: () {
-                    setState(() {
-                      _timerService.reset();
-                    });
-                  },
-                ),
-                const Text(
-                  "리셋",
-                  style: TextStyle(fontFamily: "SUITE", fontSize: 12, color: Colors.grey, fontWeight: FontWeight.w600),
-                ),
-              ],
+            IconButton(
+              icon: SvgPicture.asset(AppIcons.restartIcon),
+              iconSize: 30,
+              onPressed: () {
+                setState(() {
+                  _stopwatchService.reset();
+                });
+              },
             ),
-            const SizedBox(width: 20),
-            Column(
-              children: [
-                IconButton(
-                  icon: Icon(_timerService.isRunning ? Icons.pause : Icons.play_arrow),
-                  iconSize: 30,
-                  onPressed: () {
-                    setState(() {
-                      if (_timerService.isRunning) {
-                        _timerService.stop();
-                      } else {
-                        _timerService.start();
-                      }
-                    });
-                  },
-                ),
-                Text(
-                  _timerService.isRunning ? "정지" : "시작",
-                  style: const TextStyle(fontFamily: "SUITE", fontSize: 12, color: Colors.grey, fontWeight: FontWeight.w600),
-                ),
-              ],
+            const SizedBox(width: 40),
+            IconButton(
+              icon: SvgPicture.asset(_stopwatchService.isRunning ? AppIcons.pauseIcon : AppIcons.startIcon),
+              iconSize: 30,
+              onPressed: () {
+                setState(() {
+                  _stopwatchService.isRunning ? _stopwatchService.stop() : _stopwatchService.start();
+                });
+              },
             ),
           ],
         ),
       ],
     );
   }
+
   Widget buildSelectBook(BuildContext context) {
     return Material(
       color: Colors.transparent,
@@ -246,19 +261,11 @@ class _PomodoroTimerState extends State<PomodoroTimer> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              SvgPicture.asset(
-                'assets/images/chack_icon.svg',
-                width: 24,
-                height: 18,
-              ),
+              SvgPicture.asset('assets/images/chack_icon.svg', width: 24, height: 18),
               const SizedBox(width: 16),
               const Text(
                 '기록할 도서 선택하기',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 20,
-                  fontWeight: FontWeight.w800,
-                ),
+                style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.w800),
               ),
             ],
           ),
--- lib/services/timer_service.dart
+++ lib/services/timer_service.dart
@@ -1,33 +1,22 @@
 import 'dart:async';
 import 'dart:ui';
 
-enum TimerMode { countdown, stopwatch }
-
 class TimerService {
   late int duration;
   int _remainingTime;
   double progress;
   Timer? _timer;
   bool isRunning = false;
-  TimerMode mode;
 
-  TimerService({this.duration = 60, this.mode = TimerMode.countdown})
-      : _remainingTime = duration,
-        progress = 1.0;
-
-  // 타이머 상태 변경 리스너
   VoidCallback? onTick;
   VoidCallback? onComplete;
+
+  TimerService({this.duration = 60}) : _remainingTime = duration, progress = 1.0;
 
   void start() {
     if (isRunning) return;
     isRunning = true;
-
-    if (mode == TimerMode.countdown) {
-      _startCountdown();
-    } else if (mode == TimerMode.stopwatch) {
-      _startStopwatch();
-    }
+    _startCountdown();
   }
 
   void _startCountdown() {
@@ -35,18 +24,11 @@ class TimerService {
       if (_remainingTime > 0) {
         _remainingTime--;
         progress = _remainingTime / duration;
-        onTick?.call(); // 타이머 업데이트를 알림
+        onTick?.call(); // 타이머 업데이트
       } else {
         stop();
-        onComplete?.call(); // 타이머 완료를 알림
+        onComplete?.call(); // 타이머 완료
       }
-    });
-  }
-
-  void _startStopwatch() {
-    _timer = Timer.periodic(const Duration(milliseconds: 100), (timer) {
-      _remainingTime++;
-      onTick?.call(); // 스탑워치 업데이트를 알림
     });
   }
 
@@ -58,25 +40,15 @@ class TimerService {
 
   void reset() {
     stop();
-    if (mode == TimerMode.countdown) {
-      _remainingTime = duration;
-      progress = 1.0;
-    } else if (mode == TimerMode.stopwatch) {
-      _remainingTime = 0;
-    }
-    onTick?.call(); // 리셋 후 상태 업데이트
+    _remainingTime = duration;
+    progress = 1.0;
+    onTick?.call();
   }
 
   String formatTime() {
     final minutes = (_remainingTime ~/ 60).toString().padLeft(2, '0');
     final secs = (_remainingTime % 60).toString().padLeft(2, '0');
-
-    if (mode == TimerMode.stopwatch) {
-      final milliseconds = (_remainingTime % 1000 ~/ 10).toString().padLeft(2, '0');
-      return "$minutes:$secs.$milliseconds";
-    } else {
-      return "$minutes:$secs";
-    }
+    return "$minutes:$secs";
   }
 
   void dispose() {
